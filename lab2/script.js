f429fc44c16478f51e20e6875e83426dcde4b17e
Dmitry Maksakov (Home) <dimamax09@yandex.ru>
2020-04-14 21:29:00 +0300

    форма заработала!

---
+++ lab2/script.js
@@ -0,0 +1,14 @@
+function check() {
+    let form = document.registration;
+    let lastname = form.lastname.value;
+    let firstname = form.firstname.value;
+    let patronymic = form.patronymic.value;
+    let email = form.email.value;
+    let login = form.login.value;
+    let password = form.password.value;
+    let password2 = form.password2.value;
+    if (5<6) {
+        alert(email);
+    }
+
+}
