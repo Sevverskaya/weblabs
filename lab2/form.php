7d33f8d9099ed18b6170ddcc4aa25105cd87aa31
Dmitry <dimamax09@yandex.ru>
2020-05-10 15:13:26 +0300

    5 лаба

--- lab2/form.php
+++ lab2/form.php
@@ -121,28 +121,6 @@
     <p id="welcome" style="font-size: 55px">Добро пожаловать!</p>
     <button class="btndev" onclick="infoOpen()">Узнать больше о разработчике</button>
 </div>
-<p>
-    <?php
 
-    if (file_exists('count.txt'))
-    {
-        $fil = fopen('count.txt', r);
-        $dat = fread($fil, filesize('count.txt'));
-        echo $dat+1;
-        fclose($fil);
-        $fil = fopen('count.txt', w);
-        fwrite($fil, $dat+1);
-    }
-
-    else
-    {
-        $fil = fopen('count.txt', w);
-        fwrite($fil, 1);
-        echo '1';
-        fclose($fil);
-    }
-    ?>
-
-</p>
 </body>
 </html>
--- lab2/success.php
+++ lab2/success.php
@@ -1,3 +1,23 @@
+<?php
+
+if (file_exists('count.txt'))
+{
+    $file = fopen('count.txt', r);
+    $data = fread($file, filesize('count.txt'));
+    $result = $data + 1;
+    fclose($file);
+    $file = fopen('count.txt', w);
+    fwrite($file, $result);
+}
+
+else
+{
+    $file = fopen('count.txt', w);
+    fwrite($file, 1);
+    $result = 1;
+    fclose($file);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +28,9 @@
     <script src="script.js"></script>
 </head>
 <body class="back">
+<p> Количество посещений данной страницы: <?=$result?></p>
+<p><? if($result % 10 == 0) echo "Это ваше юбилейное посещение!"?></p>
+<p>Вы открыли эту страницу <?=date('d.m.Y в H:i:s') ?></p>
 <div class="video">
     <iframe width="640" height="350" src="https://www.youtube.com/embed/xOXfXaW0Nzw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <button onclick="confirmation()" class="btn btn-outline-secondary" style="margin-top: 30px; float: right">Закрыть</button>
